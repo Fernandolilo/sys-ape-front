f07b05bb7857b537efb140b2e45a8fc3b290a1b0
fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2024-07-06 16:58:35 -0300

    form from register

--- src/app/service/jwt.service.ts
+++ src/app/service/jwt.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const BASE_URL =  ["http://localhost:8080/*"]
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+register (singRequest : any): Observable<any>{
+  return this.http.post(BASE_URL + 'singnup', singRequest)
+}
 }
